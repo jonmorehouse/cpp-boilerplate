e65de3a0706bb02639f67cc29d1f4c7bc1312a29
Jon Morehouse <morehousej09@gmail.com>
2013-02-14 14:21:32 -0800

    Updated and fixed googletest compilation for this machine

--- test/modules/files_test.cpp
+++ test/modules/files_test.cpp
@@ -1,7 +1,7 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// #include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 // using ::testing::EmptyTestEventListener;
 // using ::testing::InitGoogleTest;
